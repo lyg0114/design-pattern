2df7baeb124809853aa1a8f8a4bff042c8b1150f
lyg0114 <whdnseowkd@naver.com>
2024-05-28 07:26:35 +0200

    feat : 어뎁터 패턴 - Tiger 클래스 도입 전

---
+++ src/main/java/com/designpatternstudy/adapter/AdapterPattern.java
@@ -0,0 +1,64 @@
+package com.designpatternstudy.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.designpatternstudy.adapter
+ * @since : 28.05.24
+ */
+
+/*
+ [어뎁터 페턴]
+  - 클래스에 대한 코드를 변경할 수 없는 상황에서 어뎁터 페턴을 사용해서 사용할 수 있다.
+ */
+public class AdapterPattern {
+
+  public static void main(String[] args) {
+    List<Animal> animals = new ArrayList<>();
+    animals.add(new Dog("댕이"));
+    animals.add(new Cat("솜털이"));
+    animals.add(new Cat("휴휴"));
+
+    for (Animal animal : animals) {
+      animal.sound();
+    }
+  }
+
+  static abstract class Animal {
+
+    protected String name;
+
+    public Animal(String name) {
+      this.name = name;
+    }
+
+    public abstract void sound();
+  }
+
+
+  static class Dog extends Animal {
+
+    public Dog(String name) {
+      super(name);
+    }
+
+    @Override
+    public void sound() {
+      System.out.println(name + " barking");
+    }
+  }
+
+  static class Cat extends Animal {
+
+    public Cat(String name) {
+      super(name);
+    }
+
+    @Override
+    public void sound() {
+      System.out.println(name + " meeow");
+    }
+  }
+}
